a59ac8dba3dc1e7d439fda24fe92b9c7ecb099a5
ALPHARESZ <jeremyalpharesz70@gmail.com>
2023-09-22 09:41:36 +0700

    kuis 1

---
+++ latihanpraktikum1nomor4.pas
@@ -0,0 +1,15 @@
+uses crt;
+var 
+a,c:string;
+b,d:integer;
+begin
+    writeln('Masukkan nama jalan rumah anda=');
+    readln(a);
+    writeln('Masukkan nomor rumah anda=');
+    readln(b);
+    writeln('Masukkan kota anda=');
+    readln(c);
+    writeln('Masukkan kode pos anda=');
+    readln(d);
+    writeln('alamat saya di jalan ',a,' No ',b,c,d)
+end.
